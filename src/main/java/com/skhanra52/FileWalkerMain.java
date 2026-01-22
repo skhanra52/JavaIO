3b17e558330295f0e5669476ae3ff82e98b5cf68
Suman Kalyan Khanra <skhanra52@gmail.com>
2026-01-22 21:22:52 +0530

    SimpleFileVisitor class example

--- src/main/java/com/skhanra52/FileWalkerMain.java
+++ src/main/java/com/skhanra52/FileWalkerMain.java
@@ -50,6 +50,57 @@ package com.skhanra52;
       TERMINATE
  */
 
-public class FileWalker {
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Objects;
+
+public class FileWalkerMain {
+
+    public static void main(String[] args) {
+
+        Path startingPath = Path.of(".");
+
+        FileVisitor<Path> statsVisitor = new StatsVisitor();
+        try {
+            Files.walkFileTree(startingPath,statsVisitor);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    private static class StatsVisitor extends SimpleFileVisitor<Path> {
+
+        int level;
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            Objects.requireNonNull(file);
+            Objects.requireNonNull(attrs);
+            System.out.println("\t".repeat(level) + file.getFileName());
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            Objects.requireNonNull(dir);
+            Objects.requireNonNull(attrs);
+
+            level++;
+            System.out.println("\t".repeat(level) + dir.getFileName());
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            Objects.requireNonNull(dir);
+//            if (exc != null)
+//                throw exc;
+            level--;
+            return FileVisitResult.CONTINUE;
+        }
+    }
 
 }
