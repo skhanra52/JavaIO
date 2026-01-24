f490fd8c95d82d43b5c7c8d6b777ff9d7a4f61ab
Suman Kalyan Khanra <skhanra52@gmail.com>
2026-01-24 07:20:06 +0530

    exmple of FileVisitor override methods

--- src/main/java/com/skhanra52/FileWalkerMain.java
+++ src/main/java/com/skhanra52/FileWalkerMain.java
@@ -92,6 +92,8 @@ Why this is important?----VVI------------------------------
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class FileWalkerMain {
@@ -112,13 +114,16 @@ public class FileWalkerMain {
 
     private static class StatsVisitor extends SimpleFileVisitor<Path> {
 
-        int level;
+        private Path initialPath = null;
+        // values of map represent the cumulative size of the folder.
+        private final Map<Path, Long> folderSizes = new LinkedHashMap<>(); // LinkedHashMap to maintain insertion order
+        private int initialCount;
 
         @Override
         public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
             Objects.requireNonNull(file);
             Objects.requireNonNull(attrs);
-            System.out.println("\t".repeat(level) + file.getFileName());
+            folderSizes.merge(file.getParent(), 0L, (o,n) -> o += attrs.size());
             return FileVisitResult.CONTINUE;
         }
 
@@ -126,18 +131,36 @@ public class FileWalkerMain {
         public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
             Objects.requireNonNull(dir);
             Objects.requireNonNull(attrs);
-
-            level++;
-            System.out.println("\t".repeat(level) + dir.getFileName());
+            if(initialPath == null){
+                initialPath = dir;
+                initialCount = dir.getNameCount();
+            } else {
+                int relativeLevel = dir.getNameCount() - initialCount;
+                if(relativeLevel == 1){
+                    folderSizes.clear();
+                }
+                folderSizes.put(dir, 0L);
+            }
             return FileVisitResult.CONTINUE;
         }
 
         @Override
         public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
             Objects.requireNonNull(dir);
-//            if (exc != null)
-//                throw exc;
-            level--;
+            if(dir.equals(initialPath)){
+                return FileVisitResult.TERMINATE;
+            }
+
+            int relativeLevel = dir.getNameCount() - initialCount;
+            if(relativeLevel == 1){
+                folderSizes.forEach((key, value) -> {
+                    int level = key.getNameCount() - initialCount  -1;
+                    System.out.printf("%s[%s] - %d byte %n", "\t".repeat(level), key.getFileName(), value);
+                });
+            }else {
+                long folderSize = folderSizes.get(dir);
+                folderSizes.merge(dir.getParent(), 0L, (o,n) -> o += folderSize);
+            }
             return FileVisitResult.CONTINUE;
         }
 
