25f8e4df7e7c47383dd583b490ac5a96e9539e73
Suman Kalyan Khanra <skhanra52@gmail.com>
2026-01-26 07:05:29 +0530

    FileReader read mechanism with File buffer

--- src/main/java/com/skhanra52/ReadingFilesMain.java
+++ src/main/java/com/skhanra52/ReadingFilesMain.java
@@ -26,13 +26,41 @@ public class ReadingFilesMain {
            A Disk read means something is physically or mechanically occurring to the hard disk to read the character
            from the file. This is expensive in terms of time and resource usages. Fortunately, java provides way to
            reduce the number of disk read being done.
-
-
          */
         try(FileReader reader = new FileReader("files/fileReading.txt")){
             int data;
             while((data = reader.read()) != -1){
                 System.out.println((char)data); // converting unsigned integers to characters.
+            }
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        /*
+         FileReader has a default buffer size, meaning it reads the certain number of character in the memory space
+         called buffer.
+         What is a File Buffer:-------------------------------
+         -> A file buffer is a computer memory which temporarily holds data while it's being read from the file.
+         -> Its primary purpose is to improve the efficiency of the data transfer and processing.
+         -> It reduces the number of direct interactions, or disk read, against the actual storage device.
+         -> In our example, where we are reading from the "files/fileReading.txt", which has 11 characters. Only one
+            of them(the first) was an actual disk read. The subsequent characters are read from the memory, or a buffer
+            read. Java's API says that the size of the buffer in the case of FileReader is implementation specific.
+            Meaning, it depends on OS and other factor.
+
+          How the FileReader actually works:---------------------
+          
+         */
+
+        try(FileReader reader = new FileReader("files/fileReading.txt")){
+            // we can read more than one character at a time, and avoid the cast,
+            // by passing a character array to the read method.
+            char[] block = new char[1000];
+            int data;
+            while((data = reader.read(block)) != -1){
+                String content = new String(block, 0, data);
+                System.out.printf("-----> [%d chars] %s%n", data, content);
             }
 
         }catch(IOException e){
