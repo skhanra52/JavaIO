58ac624ee854ab7b59b6f0e1d851535411d2c48c
Suman Kalyan Khanra <skhanra52@gmail.com>
2026-01-31 07:17:45 +0530

    File reading using Files.lines()

--- src/main/java/com/skhanra52/ReadingWithNIO2Main.java
+++ src/main/java/com/skhanra52/ReadingWithNIO2Main.java
@@ -4,6 +4,13 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ReadingWithNIO2Main {
 
@@ -24,8 +31,50 @@ public class ReadingWithNIO2Main {
         Path path = Path.of("files/fixedWidth.txt");
         try{
             System.out.println(new String(Files.readAllBytes(path)));
-            System.out.println("-Same can be done by using readString()-----------");
+            // Files.readString() also usages readAllBytes() under the hood.
+            System.out.println("Same can be done by using readString():-----------");
             System.out.println(Files.readString(path));
+
+            /*
+             We will parse the fixedWidth.txt to get distinct values out of certain column, using readAllLines(), this
+             method returns an array of String, So we will see a solution without using Stream.
+             */
+            Pattern p = Pattern.compile("(.{15})(.{3})(.{12})(.{8})(.{2}).*");
+            Set<String> values = new TreeSet<>();
+            Files.readAllLines(path).forEach(s -> {
+                if(!s.startsWith("Name")){
+                    Matcher m = p.matcher(s);
+                    if(m.matches()){
+                        values.add(m.group(3).trim());
+                    }
+                }
+            });
+            System.out.println(values);
+
+            // Another way of doing the same which we have done above, here we are using Files.lines() which returns
+            // a Stream of String.
+            try(Stream<String> stringStream = Files.lines(path)){
+                String[] result = stringStream.skip(1)
+                        .map(p::matcher)
+                        .filter(Matcher::matches)
+                        .map(m -> m.group(3).trim())
+                        .distinct()
+                        .sorted()
+                        .toArray(String[]::new);
+                System.out.println(Arrays.toString(result));
+
+            }
+
+            // We are going to count the employee department wise. Collector.groupBy() will give us a Map with department
+            // name as key and employee count as values.
+            try(Stream<String> stringStream = Files.lines(path)) {
+                var empCount = stringStream.skip(1)
+                        .map(p::matcher)
+                        .filter(Matcher::matches)
+                        .collect(Collectors.groupingBy(m -> m.group(3).trim(), Collectors.counting()));
+                empCount.entrySet().forEach(System.out::println);
+                System.out.println(empCount);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
