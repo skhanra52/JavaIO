02460460948c009ff882bd8c6eb3cfd00a6b50d7
Suman Kalyan Khanra <skhanra52@gmail.com>
2026-02-13 06:52:45 +0530

    File reading summary

--- src/main/java/com/skhanra52/readinFiles/ReadingFilesSummary.java
+++ src/main/java/com/skhanra52/readinFiles/ReadingFilesSummary.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.stream.Stream;
 
 /**
@@ -69,16 +70,67 @@ public class ReadingFilesSummary {
 
         // ----------------------- NIO way of reading file which is the newer way---------------------------------------
         System.out.println("--The newer way of reading file using Files.lines()-------");
-        /*
+
+        Path path = Path.of("files/fixedWidth.txt");
+
+        try {
+            System.out.println("Checking readString, used for smaller files");
+            System.out.println(Files.readString(path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try{
+            System.out.println("Checking readAllBytes");
+            String data = new String(Files.readAllBytes(path));
+            System.out.println(data);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+         /*
          In the below code we are using Files.lines() method which is available from java.nio.file package.
          It does open the file and read the lines, it returns a Stream<String>.
          It also read content line by line meaning, each line of the file become one String,
          and provide efficient control on encoding mechanism.
+         -> It lazy in nature, till start the consumption it does not produce stream.
+         -> Memory efficient and good for larger files.
          */
-        try(Stream<String> lines = Files.lines(Path.of("files/fixedWidth.txt"))){
+        try(Stream<String> lines = Files.lines(path)){
             lines.forEach(System.out::println);
-        } catch (Exception e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        /*
+         Here, in the below code we are checking of readAllLine() which is returning a String of List.
+         -> It loads entire file into memory, unlike Files.lines() which loads line by line.
+         -> Not efficient for larger files.
+         -> Best for small config files, and when we need access (lines.get(5))
+         */
+        try {
+            System.out.println("Checking the readAllLines");
+            List<String> lines =  Files.readAllLines(path);
+            lines.forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        /*
+         -> If you need manual control of the file content then go for newBufferReader, this is the newer way instead of
+            new BufferReader(new FileReader(path)).
+         -> Modern NIO way to creating reader.
+         */
+
+        System.out.println("Checking newBufferedRead used instead of fileReader");
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String line;
+            while((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
